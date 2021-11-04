9b1501e82fa71e107d82eee3086a8505b249d414
LitenApe <sonvo95@outlook.com>
2021-11-04 18:58:49 +0100

    chore: add quotes around out of the box

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -7,8 +7,8 @@ export function Footer() {
         The site is built using <Link href="https://nextjs.org/">nextjs</Link>{' '}
         and <Link href="https://chakra-ui.com">chakra ui</Link> with no effort
         whatsoever to the design. I did not want to waste time on choosing
-        colours, fonts and spacing so everything is built with out of the box
-        configurations.
+        colours, fonts and spacing so everything is built with &quot;out of the
+        box&quot; configurations.
       </Text>
     </Container>
   );
