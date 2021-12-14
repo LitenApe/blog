adcd8ea4321c70704b0ede2047d089380594aadf
LitenApe <sonvo95@outlook.com>
2021-12-14 19:43:00 +0100

    fix: fix link to repo

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -8,8 +8,8 @@ export function Footer() {
           The site is built using <Link href="https://nextjs.org/">nextjs</Link>{' '}
           and <Link href="https://chakra-ui.com">chakra ui</Link> with no effort
           whatsoever to the design. The entire site can be found on{' '}
-          <Link href="https://github.com/LitenApe/blog">github</Link> if you are
-          curious about how the site was created and configured.
+          <Link href="https://github.com/LitenApe/sonthanhvo">github</Link> if
+          you are curious about how the site was created and configured.
         </Text>
       </Box>
     </Container>
