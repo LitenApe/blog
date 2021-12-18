86199aecf7c51f89e216ec0ff5dc41b1b1b085ee
LitenApe <sonvo95@outlook.com>
2021-12-18 14:23:24 +0100

    style: add margin to headings

--- utils/processRawContent.ts
+++ utils/processRawContent.ts
@@ -24,11 +24,15 @@ const processor = unified()
     createElement: createElement,
     components: {
       h1: (props) =>
-        createElement(Heading, { ...props, as: 'h1', size: '2xl' }),
-      h2: (props) => createElement(Heading, { ...props, as: 'h2', size: 'xl' }),
-      h3: (props) => createElement(Heading, { ...props, as: 'h3', size: 'lg' }),
-      h4: (props) => createElement(Heading, { ...props, as: 'h4', size: 'md' }),
-      h5: (props) => createElement(Heading, { ...props, as: 'h5', size: 'sm' }),
+        createElement(Heading, { ...props, as: 'h1', size: '2xl', my: 6 }),
+      h2: (props) =>
+        createElement(Heading, { ...props, as: 'h2', size: 'xl', my: 4 }),
+      h3: (props) =>
+        createElement(Heading, { ...props, as: 'h3', size: 'lg', my: 4 }),
+      h4: (props) =>
+        createElement(Heading, { ...props, as: 'h4', size: 'md', my: 4 }),
+      h5: (props) =>
+        createElement(Heading, { ...props, as: 'h5', size: 'sm', my: 4 }),
       p: Paragraph,
       a: (props) =>
         createElement(Link, { ...props, textDecoration: 'underline' }),
