7702c899420ceee392f9d73ddaa6d5351861d8c1
LitenApe <sonvo95@outlook.com>
2021-12-30 12:21:00 +0100

    feat: add color theme based on system

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,18 @@
 import '../styles/index.scss';
 import 'normalize.css';
 
-import { ChakraProvider, Divider } from '@chakra-ui/react';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import type { AppProps } from 'next/app';
 import { Footer } from '../components/layout/Footer';
 import { Navigation } from '../components/layout/Navigation';
 
+import { ColorModeScript } from '@chakra-ui/react';
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <ColorModeScript initialColorMode="system" />
       <Navigation />
       <Component {...pageProps} />
       <Footer />
